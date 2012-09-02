8f30f3e647b0449dbbbf8c612702fb9e8d983112
Jason <mcmonkeygames@gmail.com>
2012-09-01 20:22:47 -0700

    Initial commit

---
+++ src/net/cloudapp/mcminecraftwest/bukkit/mcwater/McWater.java
@@ -0,0 +1,11 @@
+package net.cloudapp.mcminecraftwest.bukkit.mcwater;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jason
+ * Date: 9/1/12
+ * Time: 8:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class McWater {
+}
